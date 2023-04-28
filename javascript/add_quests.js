e4363d51de29b6ae812946cea0874c314f404c7d
Kenneth Romero <romero.kennethf@gmail.com>
2023-04-28 00:36:35 -0400

    changed nav bars, fixed sidebars

--- javascript/add_quests.js
+++ javascript/add_quests.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const questName = document.getElementById('quest-name').value;
     const questReward = document.getElementById('quest-reward').value;
     const questDesc = document.getElementById('quest-desc').value;
+    const questLocation = document.getElementById('quest-location').value || 'Unknown';
 
     const questItem = document.createElement('div');
     questItem.className = 'made-quests';
@@ -17,6 +18,8 @@ document.addEventListener('DOMContentLoaded', () => {
         <h4>${questName}</h4>
         <p>Reward: ${questReward}</p>
         <p>${questDesc}</p>
+        <p>Location : ${questLocation}</p>
+        <button class="pursue-quest">Pursue</button>
         <button class="complete-quest">Complete</button>
         <button class="delete-quest">Delete</button>
       `;
